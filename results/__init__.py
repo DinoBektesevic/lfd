de04e9cef1adcfa9dff37f19132d2d17ef57e2c8
DinoBektesevic <dinob@uw.edu>
2018-05-23 01:40:24 -0700

    Cleaning up the results modules.

--- results/__init__.py
+++
@@ -1,7 +0,0 @@
-from .line import Line
-from .point import Point
-import ccd_dimensions as ccd
-from frame import Frame
-from .coord_conversion import *
-from results import Result, Results
-#from .mwe import *
