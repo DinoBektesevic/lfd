5f9b4e1d1b90f751c360928d604b3b9db0d1e440
DinoBektesevic <dinob@uw.edu>
2019-01-15 14:44:08 -0800

    Fixed wrong path in setup.py.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
@@ -10,7 +10,7 @@ setup(
     version='1.0',
     description='Linear Feature Detector for Astronomical images',
     long_description=long_description,
-    long_description_content_type='text/markdown'
+    long_description_content_type='text/markdown',
     author='Dino Bektesevic',
     author_email='ljetibo@gmail.com',
     packages=find_packages(),  #same as name
