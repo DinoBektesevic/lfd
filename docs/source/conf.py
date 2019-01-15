f2935c5f6edc68a48de3b08c93716cbb2ca8fe89
DinoBektesevic <dinob@uw.edu>
2019-01-14 17:31:21 -0800

    Removing mock code. Attempt 16

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,19 +24,6 @@
 #sys.path.insert(0, os.path.abspath("/home/dino/Desktop/lfd"))
 #sys.path.insert(0, os.path.abspath('.'))
 
-
-import sys
-from unittest.mock import MagicMock
-
-class Mock(MagicMock):
-    @classmethod
-    def __getattr__(cls, name):
-        return MagicMock()
-
-MOCK_MODULES = ['cv2', 'opencv-python', 'fitsio']
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
-
-
 # -- Project information -----------------------------------------------------
 project = 'lfd'
 copyright = '2019, Dino Bektesevic'
